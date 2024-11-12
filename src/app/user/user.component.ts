6c80ec36d62284471b9de977323fe2882c801654
Mateusz Urbaniak <matteus.urbaniak@hotmail.com>
2024-11-12 16:37:19 +0100

    Add Input decorator for sharing parents-child

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,6 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, Input, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -11,17 +10,11 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal( DUMMY_USERS[randomIndex]);
 
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+  @Input() avatar!: string 
+  @Input() name!: string 
 
-  // get imagePath() {
-  //   return 'assets/users/' + this.selectedUser.avatar;
-  // }
+  onSelectUser() {}
 
-  onSelectuser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
-    this.selectedUser.set(DUMMY_USERS[randomIndex])
-  }
 }
