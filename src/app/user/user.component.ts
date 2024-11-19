090d0ab9a8d990fee042ca60f6683594e841e64e
Mateusz Urbaniak <matteus.urbaniak@hotmail.com>
2024-11-19 20:26:00 +0100

    Implementing @Inputs

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,8 +11,12 @@ import { DUMMY_USERS } from '../dummy-users';
 })
 export class UserComponent {
 
-  @Input() avatar!: string 
-  @Input() name!: string 
+  @Input({required: true}) avatar: string 
+  @Input({required: true}) name: string 
+
+  get imagePath(): string {
+    return '/assets/users/' + this.avatar
+  }
 
   onSelectUser() {}
 
