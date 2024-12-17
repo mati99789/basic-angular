bd27a02b2ed003c53e103c16a5c82b4956756248
Mateusz Urbaniak <matteus.urbaniak@hotmail.com>
2024-12-17 17:04:42 +0100

    Feat: Implement two ways binding via formModel.

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,15 +1,22 @@
 import {Component, EventEmitter, Output} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
 
   @Output() cancel = new EventEmitter<void>();
+  enteredTitle = ""
+  enteredSummary =""
+  enteredDate=""
+
 
   onClose() {
     this.cancel.emit();
